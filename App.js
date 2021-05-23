6bc04ca26c02d6a02d11db8a09fb9c8f83477829
unknown <zim21den21@gmail.com>
2021-05-23 17:56:32 +0300

    init

--- App.js
+++ App.js
@@ -1,21 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react"
+import { Button, StyleSheet, Text, View, FlatList } from "react-native"
+import Header from "./comp/Header"
+import ToDoItem from "./comp/ToDoItem"
 
 export default function App() {
+  const [data, setData] = useState([
+    { text: "buy something", key: "1" },
+    { text: "smoke shisha", key: "2" },
+    { text: "create native app", key: "3" },
+  ])
+
+  const pressHandel = (key) => {
+    setData((prev) => {
+      return prev.filter((item) => item.key !== key)
+    })
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header />
+      <View style={styles.content}>
+        <View style={styles.list}>
+          <FlatList
+            data={data}
+            renderItem={({ item }) => (
+              <ToDoItem item={item} pressHandel={pressHandel}>
+                {item.text}
+              </ToDoItem>
+            )}
+          />
+        </View>
+      </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
   },
-});
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20,
+  },
+})
